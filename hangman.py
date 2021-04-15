77d619d174f763947d78c41a4865be1a6ca4147c
apawate <apawate739@student.fuhsd.org>
2021-04-15 16:35:19 -0700

    Finished the basic version of the game. That was quicker than expected.

--- hangman.py
+++ hangman.py
@@ -14,9 +14,12 @@ hardwords = ["application", "rhythm", "xylophone", "lightning", "quizzical", "ra
 
 crazywords = ["prophaeletherium", "diophantus", "supercalifragilisticexpialidocious", "teleconferencing", "deoxyribonucleic", "developmentally", "metallurgical"]
 
+optionwords = [easywords, mediumwords, hardwords, crazywords]
+
 print("Let's play hangman!")
 choice = input("Choose a level: 1 is easy, 2 is medium, 3 is hard, and don't choose 4. " )
-print("You chose the", options[int(choice) - 1], "level. Good luck!")
+choice = int(choice) - 1
+print("You chose the", options[choice], "level. Good luck!")
 time.sleep(1)
 os.system("clear")
 
@@ -36,13 +39,57 @@ ropesix = " ______\n/      |\n|     [_]\n|      | \n|    -- --\n|      |\n|     
 
 ropefive = " ______\n/      |\n|     [_]\n|      | \n|    -- --\n|      |\n|     / \n|    /  "
 
-ropelist = ["\n\n\n\n\n\n\n", ropezero, ropeone, ropetwo, ropethree, ropefour, ropefive, ropesix]
+ropelist = ["\n\n\n\n\n\n\n", "\n\n\n\n\n\n\n", ropezero, ropeone, ropetwo, ropethree, ropefour, ropefive, ropesix]
 
-for rope in ropelist:
+gallows_state = False
+
+round_num = 0
+
+guess = ""
+
+guessletters = []
+
+word = random.choice(optionwords[choice])
+
+while guess != "quit":
     os.system("clear")
-    print(rope)
-    print(gallows)
-    time.sleep(1)
+    print(ropelist[round_num])
+    if gallows_state:
+        print(gallows)
+    else:
+        print()
+    if round_num == 8:
+        print("Game over!")
+        print("The word was", word)
+        guess = "quit"
+    else:
+        for letter in word:
+            if letter not in guessletters:
+                print("_", end="")
+            else:
+                print(letter, end="")
+        print()
+        guess = input("Enter a guess: ")
+        if len(guess) == 1:
+            guessletters.append(guess)
+            if guess not in word:
+                if not gallows_state:
+                    gallows_state = True
+                round_num = round_num + 1
+        else:
+            if guess != word:
+                if not gallows_state:
+                    gallows_state = True
+                round_num = round_num + 1
+            else:
+                os.system("clear")
+                print("You won!")
+                guess = "quit"
+
+
+
+    
+
 
        
 
