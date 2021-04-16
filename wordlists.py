c23fd27e1a85233a05ba7d3d50a72c381c88124c
apawate <apawate739@student.fuhsd.org>
2021-04-16 14:47:22 -0700

    Added to the wordlists.

--- wordlists.py
+++ wordlists.py
@@ -1,9 +1,9 @@
 class words:
     def __init__(self):
-        self.easywords = ["bat", "rat", "cat", "dog", "big", "hello", "word", "easy", "phone", "man", "woman", "book", "door", "wind", "tree", "hot", "mouse", "cake", "coffee", "eat", "plate"]
+        self.easywords = ["bat", "rat", "cat", "dog", "big", "hello", "word", "easy", "phone", "man", "woman", "book", "door", "wind", "tree", "hot", "mouse", "cake", "coffee", "eat", "plate", "bird", "bag"]
 
-        self.mediumwords = ["frame", "computer", "apply", "world", "wallpaper", "notebook", "ocean", "waves", "handle", "chess", "calendar", "binder", "heater", "light", "knife", "click", "construction"]
+        self.mediumwords = ["frame", "computer", "apply", "world", "wallpaper", "notebook", "ocean", "waves", "handle", "chess", "calendar", "binder", "heater", "light", "knife", "click", "construction", "pencil", "heron", "text", "agenda", "clock", "terminal", "screen", "liquid", "crystal"]
 
-        self.hardwords = ["application", "rhythm", "xylophone", "lightning", "quizzical", "radioed", "architect", "sequoia", "tyrannical", "quirkiness", "bicycle", "periodic", "methodology"]
+        self.hardwords = ["application", "rhythm", "xylophone", "lightning", "quizzical", "radioed", "architect", "sequoia", "tyrannical", "quirkiness", "bicycle", "periodic", "methodology", "mockingbird", "mechanism", "thrifty", "brought", "cyclic"]
 
-        self.crazywords = ["prophaeletherium", "velociraptor", "supercalifragilisticexpialidocious", "teleconferencing", "deoxyribonucleic", "developmentally", "metallurgical", "neurosurgeon", "cerebellum", "antebellum"]
+        self.crazywords = ["prophaeletherium", "velociraptor", "supercalifragilisticexpialidocious", "teleconferencing", "deoxyribonucleic", "developmentally", "metallurgical", "neurosurgeon", "cerebellum", "antebellum", "encyclopedia", "bibliographical", "autobiographically", "antidisestablishmentarianism", "triskadekaphobia"]
