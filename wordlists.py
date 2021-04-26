3f6ee7442c3dafd65b0bda92ec7649a661ed8209
Agastya Pawate <73004127+apawate@users.noreply.github.com>
2021-04-26 13:35:49 -0700

    Made the wordlists secrets.

--- wordlists.py
+++ wordlists.py
@@ -1,9 +1,6 @@
 class words:
     def __init__(self):
-        self.easywords = ["bat", "rat", "cat", "dog", "big", "hello", "word", "easy", "phone", "man", "woman", "book", "door", "wind", "tree", "hot", "mouse", "cake", "coffee", "eat", "plate", "bird", "bag"]
-
-        self.mediumwords = ["frame", "computer", "apply", "world", "wallpaper", "notebook", "ocean", "waves", "handle", "chess", "calendar", "binder", "heater", "light", "knife", "click", "construction", "pencil", "heron", "text", "agenda", "clock", "terminal", "screen", "liquid", "crystal"]
-
-        self.hardwords = ["application", "rhythm", "xylophone", "lightning", "quizzical", "radioed", "architect", "sequoia", "tyrannical", "quirkiness", "bicycle", "periodic", "methodology", "mockingbird", "mechanism", "thrifty", "brought", "cyclic"]
-
-        self.crazywords = ["prophaeletherium", "velociraptor", "supercalifragilisticexpialidocious", "teleconferencing", "deoxyribonucleic", "developmentally", "metallurgical", "neurosurgeon", "cerebellum", "antebellum", "encyclopedia", "bibliographical", "autobiographically", "antidisestablishmentarianism", "triskadekaphobia"]
+        self.easywords = EASY_WORDS
+        self.mediumwords = MEDIUM_WORDS
+        self.hardwords = HARD_WORDS
+        self.crazywords = CRAZY_WORDS
