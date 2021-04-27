1e2576c34f1fbb05c503d9ca836f7ea4498f0106
Agastya Pawate <73004127+apawate@users.noreply.github.com>
2021-04-27 09:14:16 -0700

    Gave up on making the wordlists secrets (for now).

--- wordlists.py
+++ wordlists.py
@@ -1,8 +1,9 @@
-import os
-
 class words:
     def __init__(self):
-        self.easywords = os.getenv('EASY_WORDS')
-        self.mediumwords = os.getenv('MEDIUM_WORDS')
-        self.hardwords = os.getenv('HARD_WORDS')
-        self.crazywords = os.getenv('CRAZY_WORDS')
+        self.easywords = ["bat", "rat", "cat", "dog", "big", "hello", "word", "easy", "phone", "man", "woman", "book", "door", "wind", "tree", "hot", "mouse", "cake", "coffee", "eat", "plate", "bird", "bag"]
+
+        self.mediumwords = ["frame", "computer", "apply", "world", "wallpaper", "notebook", "ocean", "waves", "handle", "chess", "calendar", "binder", "heater", "light", "knife", "click", "construction", "pencil", "heron", "text", "agenda", "clock", "terminal", "screen", "liquid", "crystal"]
+
+        self.hardwords = ["application", "rhythm", "xylophone", "lightning", "quizzical", "radioed", "architect", "sequoia", "tyrannical", "quirkiness", "bicycle", "periodic", "methodology", "mockingbird", "mechanism", "thrifty", "brought", "cyclic"]
+
+        self.crazywords = ["prophaeletherium", "velociraptor", "supercalifragilisticexpialidocious", "teleconferencing", "deoxyribonucleic", "developmentally", "metallurgical", "neurosurgeon", "cerebellum", "antebellum", "encyclopedia", "bibliographical", "autobiographically", "antidisestablishmentarianism", "triskadekaphobia"]
