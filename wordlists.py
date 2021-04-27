0cbbc3a3a930c77db1fd577e25280199a7dec627
Agastya Pawate <73004127+apawate@users.noreply.github.com>
2021-04-27 09:17:59 -0700

    Added more words to the wordlists.

--- wordlists.py
+++ wordlists.py
@@ -2,8 +2,8 @@ class words:
     def __init__(self):
         self.easywords = ["bat", "rat", "cat", "dog", "big", "hello", "word", "easy", "phone", "man", "woman", "book", "door", "wind", "tree", "hot", "mouse", "cake", "coffee", "eat", "plate", "bird", "bag"]
 
-        self.mediumwords = ["frame", "computer", "apply", "world", "wallpaper", "notebook", "ocean", "waves", "handle", "chess", "calendar", "binder", "heater", "light", "knife", "click", "construction", "pencil", "heron", "text", "agenda", "clock", "terminal", "screen", "liquid", "crystal"]
+        self.mediumwords = ["frame", "computer", "helmet", "keyboard", "table", "apply", "world", "wallpaper", "notebook", "ocean", "waves", "handle", "chess", "calendar", "binder", "heater", "light", "knife", "click", "construction", "pencil", "heron", "text", "agenda", "clock", "terminal", "screen", "liquid", "crystal"]
 
-        self.hardwords = ["application", "rhythm", "xylophone", "lightning", "quizzical", "radioed", "architect", "sequoia", "tyrannical", "quirkiness", "bicycle", "periodic", "methodology", "mockingbird", "mechanism", "thrifty", "brought", "cyclic"]
+        self.hardwords = ["application", "rhythm", "xylophone", "lightning", "blythe", "quizzically", "radioed", "architect", "sequoia", "tyrannical", "quirkiness", "bicycle", "periodic", "methodology", "mockingbird", "mechanism", "thrifty", "brought", "cyclic"]
 
-        self.crazywords = ["prophaeletherium", "velociraptor", "supercalifragilisticexpialidocious", "teleconferencing", "deoxyribonucleic", "developmentally", "metallurgical", "neurosurgeon", "cerebellum", "antebellum", "encyclopedia", "bibliographical", "autobiographically", "antidisestablishmentarianism", "triskadekaphobia"]
+        self.crazywords = ["prophaeletherium", "australopithecine", "velociraptor", "supercalifragilisticexpialidocious", "teleconferencing", "deoxyribonucleic", "developmentally", "metallurgical", "neurosurgeon", "cerebellum", "antebellum", "encyclopedia", "bibliographical", "autobiographically", "antidisestablishmentarianism", "triskadekaphobia"]
